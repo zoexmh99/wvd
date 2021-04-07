8b6f7293f771f8b1fb964a032197dacc29f67ae0
soju6jan <cybersol@naver.com>
2021-04-08 04:33:11 +0900

    1

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 #from .plugin import blueprint, menu, plugin_load, plugin_unload, plugin_info
 import os
-    from framework import app
+from framework import app
+
 try:
     from selenium import webdriver
 except:
